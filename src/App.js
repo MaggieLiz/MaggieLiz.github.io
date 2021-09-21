518a5d9d0f1a1cc7d3231de86afd16d1b5571a69
maggieliz <maggielizward@gmail.com>
2021-09-21 13:08:27 +0100

    Setup vscode and got a home page and navbar.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
+  
+  )
+}
+
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <section>
+      <h1>hello world</h1>
+      <p>this is maggie's portfolio page</p>
+    </section>
+  )
+}
+
+export default Home
+
+
