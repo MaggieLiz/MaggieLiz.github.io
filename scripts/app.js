21098d5889da56f2b90594f8b6146c7fa0ee557b
maggieliz <maggielizward@gmail.com>
2021-10-07 13:52:07 +0100

    Added media queries and changed handleClick functions a little on interests.

--- scripts/app.js
+++ scripts/app.js
@@ -8,19 +8,22 @@ const sewingDescription = document.querySelector('.sewing-description')
 const readingDiv = document.querySelector('#reading')
 const readingDescription = document.querySelector('.reading-description')
 
-function handleFitnessClick(e) {
-  console.log(e)
+function handleFitnessClick() {
   fitnessDescription.classList.toggle('show')
+  sewingDiv.classList.toggle('hide')
+  readingDiv.classList.toggle('hide')
 }
 
-function handleSewingClick(e) {
-  console.log(e)
+function handleSewingClick() {
   sewingDescription.classList.toggle('show')
+  fitnessDiv.classList.toggle('hide')
+  readingDiv.classList.toggle('hide')
 }
 
-function handleReadingClick(e) {
-  console.log(e)
+function handleReadingClick() {
   readingDescription.classList.toggle('show')
+  fitnessDiv.classList.toggle('hide')
+  sewingDiv.classList.toggle('hide')
 }
 
 fitnessDiv.addEventListener('click', handleFitnessClick)
