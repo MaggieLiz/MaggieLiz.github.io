aa6a40f6e6d84a4c04d32164a3a06faaeec63e1d
maggieliz <maggielizward@gmail.com>
2021-10-07 12:38:28 +0100

    Added styling and event listeners to the interests. Still don't like it.

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,28 @@
-console.log('hello world')
 
-window.addEventListener("load", function(){
-	const delay = 2;
-	const nodes = document.querySelectorAll
-(".animate");
-	for(let i=0; i<nodes.length; i++){
-		const words = nodes[i].innerText.split(" ");
-		nodes[i].innerHTML = "";
-for(let i2=0; i2<words.length; i2++){
-			const item = document.createElement("span");
-			item.innerText = words[i2];
-			const calc = (delay+((nodes.length + i2)/3));
-	item.style.animationDelay = calc+"s";
-			nodes[i].appendChild(item);
+const fitnessDiv = document.querySelector('#fitness')
+const fitnessDescription = document.querySelector('.fitness-description')
+
+const sewingDiv = document.querySelector('#sewing')
+const sewingDescription = document.querySelector('.sewing-description')
+
+const readingDiv = document.querySelector('#reading')
+const readingDescription = document.querySelector('.reading-description')
+
+function handleFitnessClick(e) {
+  console.log(e)
+  fitnessDescription.classList.toggle('show')
 }
-	}
-});
+
+function handleSewingClick(e) {
+  console.log(e)
+  sewingDescription.classList.toggle('show')
+}
+
+function handleReadingClick(e) {
+  console.log(e)
+  readingDescription.classList.toggle('show')
+}
+
+fitnessDiv.addEventListener('click', handleFitnessClick)
+sewingDiv.addEventListener('click', handleSewingClick)
+readingDiv.addEventListener('click', handleReadingClick)
