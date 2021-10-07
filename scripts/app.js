848ba6de887e1ab6e2a8ba9c0a005a05838db456
maggieliz <maggielizward@gmail.com>
2021-10-07 10:09:42 +0100

    Added styling to majority of page. I don't love anything about it, but it will do for now. Need to finish styling the interests section and the footer.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
 console.log('hello world')
+
+window.addEventListener("load", function(){
+	const delay = 2;
+	const nodes = document.querySelectorAll
+(".animate");
+	for(let i=0; i<nodes.length; i++){
+		const words = nodes[i].innerText.split(" ");
+		nodes[i].innerHTML = "";
+for(let i2=0; i2<words.length; i2++){
+			const item = document.createElement("span");
+			item.innerText = words[i2];
+			const calc = (delay+((nodes.length + i2)/3));
+	item.style.animationDelay = calc+"s";
+			nodes[i].appendChild(item);
+}
+	}
+});
